5fbfadd90c4acc2577412bbec7dece01e725f63b
kikeyama <ikeyama.x.knhk@gmail.com>
2020-06-09 23:01:57 +0900

    add /error

--- src/main/java/com/example/demo/DemoRest.java
+++ src/main/java/com/example/demo/DemoRest.java
@@ -1,14 +1,20 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.lang.NullPointerException;
 
 @RestController
 public class DemoRest {
 
-	@RequestMapping("/")
+	@GetMapping("/")
 	public String index() {
 		return "Greetings from Spring Boot!";
+	}
+
+	@GetMapping("/error")
+	public String error() {
+		throw new NullPointerException("ぬるぽ");
 	}
 
 }
