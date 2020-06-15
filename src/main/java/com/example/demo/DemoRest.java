63b609c19003e3a6e1342615dafd40991ee94f2e
kikeyama <ikeyama.x.knhk@gmail.com>
2020-06-15 15:06:06 +0900

    add Flask endpoint

--- src/main/java/com/example/demo/DemoRest.java
+++ src/main/java/com/example/demo/DemoRest.java
@@ -2,7 +2,14 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.client.RestTemplate;
+//import org.springframework.beans.factory.annotation.Value;
+import org.json.simple.JSONObject;
 import java.lang.NullPointerException;
+import java.lang.IllegalArgumentException;
 
 @RestController
 public class DemoRest {
@@ -15,6 +22,37 @@ public class DemoRest {
 	@GetMapping("/error")
 	public String error() {
 		throw new NullPointerException("ぬるぽ");
+	}
+
+	@GetMapping("/api/demo")
+	public String demo(@RequestParam String status) {
+		if ("success".equals(status)) {
+			return "Success!";
+		} else if ("error".equals(status)) {
+			throw new IllegalArgumentException("エラーでんがな");
+		} else {
+			return "ちゃんとクエリーストリング `status` 入れてや";
+		}
+	}
+
+	@PostMapping("/api/post")
+	public String post(@RequestBody JSONObject body) {
+		return body.toString();
+	}
+
+	@GetMapping("/api/flask")
+	public String requestFlask(@RequestParam String name) {
+		
+//		@Value("#{environment.FLASK_HOST}")
+//		String flaskHost;    // localhost:5050
+
+		String flaskHost = System.getenv("FLASK_HOST");
+
+		RestTemplate restTemplate = new RestTemplate();
+
+		String flaskEndpoint = "http://" + flaskHost + "/?name=" + name;
+		String result = restTemplate.getForObject(flaskEndpoint, String.class);
+		return result;
 	}
 
 }
