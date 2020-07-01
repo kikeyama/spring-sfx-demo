7db483f7d7f4a3b42344d31983d67c4b642ad794
kikeyama <ikeyama.x.knhk@gmail.com>
2020-07-02 02:12:42 +0900

    add logback

--- src/main/java/com/example/demo/DemoRest.java
+++ src/main/java/com/example/demo/DemoRest.java
@@ -11,22 +11,32 @@ import org.json.simple.JSONObject;
 import java.lang.NullPointerException;
 import java.lang.IllegalArgumentException;
 import org.apache.commons.lang3.RandomStringUtils;
+//Logging by Logback
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 public class DemoRest {
 
+	public static Logger logger = LoggerFactory.getLogger(DemoRest.class);
+
 	@GetMapping("/")
 	public String index() {
+		logger.info("root endpoint");
 		return "Greetings from Spring Boot!";
 	}
 
 	@GetMapping("/error")
 	public String error() {
+		logger.info("error endpoint");
 		throw new NullPointerException("ぬるぽ");
 	}
 
 	@GetMapping("/api/demo")
 	public String demo(@RequestParam String status) {
+
+		logger.info("demo endpoint with query: status=" + status);
+
 		if ("success".equals(status)) {
 			return "Success!";
 		} else if ("error".equals(status)) {
@@ -38,6 +48,7 @@ public class DemoRest {
 
 	@PostMapping("/api/post")
 	public String post(@RequestBody JSONObject body) {
+		logger.info("post endpoint with post: body" + body.toString());
 		return body.toString();
 	}
 
@@ -47,11 +58,14 @@ public class DemoRest {
 //		@Value("#{environment.FLASK_HOST}")
 //		String flaskHost;    // localhost:5050
 
+		logger.info("flask endpoint");
 		String flaskHost = System.getenv("FLASK_HOST");
 
 		RestTemplate restTemplate = new RestTemplate();
 
 		String flaskEndpoint = "http://" + flaskHost + "/?name=" + name;
+
+		logger.info("call flask endpoint");
 		String result = restTemplate.getForObject(flaskEndpoint, String.class);
 		return result;
 	}
@@ -59,6 +73,7 @@ public class DemoRest {
 	@GetMapping("/api/gorilla/id")
 	public String requestGorilla(@RequestParam String httpStatus) {
 		
+		logger.info("gorilla id endpoint");
 		String gorillaHost = System.getenv("GORILLA_HOST");
 		String randomTraceId = RandomStringUtils.randomAlphanumeric(16).toLowerCase();
 
