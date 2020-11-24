f513a4f6bd7fd301409670fb6f16691ff1ae0494
kikeyama <kikeyama@users.noreply.github.com>
2020-11-25 01:03:35 +0900

    add jdbc postgresql

--- src/main/java/com/example/demo/DemoRest.java
+++ src/main/java/com/example/demo/DemoRest.java
@@ -14,11 +14,25 @@ import org.apache.commons.lang3.RandomStringUtils;
 //Logging by Logback
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+//JDBC Driver
+//mport java.sql.Connection;
+//mport java.sql.DriverManager;
+//mport java.sql.ResultSet;
+//mport java.sql.SQLException;
+//mport java.sql.Statement;
+// Spring JDBC
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class DemoRest {
 
 	public static Logger logger = LoggerFactory.getLogger(DemoRest.class);
+
+	@Autowired
+	JdbcTemplate jdbcTemplate;
 
 	@GetMapping("/")
 	public String index() {
@@ -88,6 +102,64 @@ public class DemoRest {
 		String gorillaEndpoint = "http://" + gorillaHost + "/api/trace/" + randomTraceId + "?httpstatus=" + httpStatus;
 		String result = restTemplate.getForObject(gorillaEndpoint, String.class);
 		return result;
+	}
+
+	@GetMapping("/api/postgresql")
+	public String postgresql(@RequestParam String name) {
+		
+		logger.info("postgresql endpoint");
+
+		List<Map<String,Object>> list;
+		list = jdbcTemplate.queryForList("SELECT UUID, NAME, NUMBER FROM SAMPLE WHERE NAME=?", name);
+		return list.toString();
+
+//		String pgHost = System.getenv().getOrDefault("POSTGRESQL_HOST", "localhost");
+//		String pgPort = System.getenv().getOrDefault("POSTGRESQL_PORT", "5432");
+//		String pgDB = System.getenv().getOrDefault("POSTGRESQL_DATABASE", "kikeyama");
+//		String pgUser = System.getenv().getOrDefault("POSTGRESQL_USERNAME", "kikeyama");
+//		String pgPassword = System.getenv().getOrDefault("POSTGRESQL_PASSWORD", "password");
+//
+//		// JDBC Url
+//		String url = String.format("jdbc:postgresql://%s:%s/%s", pgHost, pgPort, pgDB);
+//
+//		// Declare blank connections
+//		Connection conn = null;
+//		Statement stmt = null;
+//		ResultSet rset = null;
+//
+//		try{
+//			// Connect to PostgreSQL
+//			conn = DriverManager.getConnection(url, pgUser, pgPassword);
+//
+//			// Turn off auto-commit
+//			conn.setAutoCommit(false);
+//
+//			// Execute SELECT statement
+//			stmt = conn.createStatement();
+//			String sql = String.format("SELECT UUID, NAME, NUMBER FROM SAMPLE WHERE NAME='%s'", name);
+//			rset = stmt.executeQuery(sql);
+//
+//			// SELECT ResultSet
+//			while(rset.next()){
+//				String uuid = rset.getString(1);
+//				logger.info("name=" + name + " uuid=" + uuid);
+//			}
+//		}
+//		catch (SQLException e){
+//			e.printStackTrace();
+//		}
+//		finally {
+//			try {
+//				if(rset != null)rset.close();
+//				if(stmt != null)stmt.close();
+//				if(conn != null)conn.close();
+//			}
+//			catch (SQLException e){
+//				e.printStackTrace();
+//			}
+//
+//		}
+//		return null;
 	}
 
 }
